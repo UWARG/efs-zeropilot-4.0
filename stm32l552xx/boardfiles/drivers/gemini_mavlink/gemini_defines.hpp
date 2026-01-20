ff3f445eebe94e0cdc5412770202c24c40e0d0b9
wandouuu <130308899+wandouuu@users.noreply.github.com>
2026-01-19 23:59:31 -0500

    Add MAVLINK_CHANNEL constexpr

--- stm32l552xx/boardfiles/drivers/gemini_mavlink/gemini_defines.hpp
+++ stm32l552xx/boardfiles/drivers/gemini_mavlink/gemini_defines.hpp
@@ -2,5 +2,7 @@
 
 #include "stm32l5xx.h"
 #include <cstdint>
+#include "mavlink.h"
 
 static constexpr uint16_t MAVLINK_MAX_PACKET_SIZE = 280; 
+static constexpr uint8_t MAVLINK_CHANNEL = MAVLINK_COMM_0;
