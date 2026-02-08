519881386682f108f72dd881a4ccbe94e577fc8e
Niran-dev1 <96404531+Niranjan-J1@users.noreply.github.com>
2026-02-07 20:29:30 -0500

    battery failsafe logic w/ persistence
    The last commit was .hpp file by accident, this one is the correct one

--- zeropilot4.0/src/system_manager/system_manager.cpp
+++ zeropilot4.0/src/system_manager/system_manager.cpp
@@ -1,64 +1,192 @@
-#pragma once
+#include "system_manager.hpp"
 
-#include "iwdg_iface.hpp"
-#include "systemutils_iface.hpp"
-#include "mavlink.h"
-#include "logger_iface.hpp"
-#include "rc_iface.hpp"
-#include "rc_motor_control.hpp"
-#include "iwdg_iface.hpp"
-#include "tm_queue.hpp"
-#include "queue_iface.hpp"
-#include "power_module_iface.hpp"
+#define SM_SCHEDULING_RATE_HZ 20
+#define SM_TELEMETRY_HEARTBEAT_RATE_HZ 1
+#define SM_TELEMETRY_RC_DATA_RATE_HZ 5
 
-#define SM_CONTROL_LOOP_DELAY 50
-#define SM_RC_TIMEOUT 500 
+#define BATTERY_LOW_VOLTAGE      10.5f
+#define BATTERY_CRITICAL_VOLTAGE 9.8f
 
-class SystemManager {
-    public:
-        SystemManager(
-            ISystemUtils *systemUtilsDriver,
-            IIndependentWatchdog *iwdgDriver,
-            ILogger *loggerDriver,
-            IRCReceiver *rcDriver,
-			IPowerModule *pmDriver,
-            IMessageQueue<RCMotorControlMessage_t> *amRCQueue,
-            IMessageQueue<TMMessage_t> *tmQueue,
-            IMessageQueue<char[100]> *smLoggerQueue
-        );
+#define BATTERY_LOW_TIME_MS      10000
+#define BATTERY_CRITICAL_TIME_MS 3000
 
-        void smUpdate(); // This function is the main function of SM, it should be called in the main loop of the system.
+SystemManager::SystemManager(
+    ISystemUtils *systemUtilsDriver,
+    IIndependentWatchdog *iwdgDriver,
+    ILogger *loggerDriver,
+    IRCReceiver *rcDriver,
+	IPowerModule *pmDriver,
+    IMessageQueue<RCMotorControlMessage_t> *amRCQueue,
+    IMessageQueue<TMMessage_t> *tmQueue,
+    IMessageQueue<char[100]> *smLoggerQueue) :
+        systemUtilsDriver(systemUtilsDriver),
+        iwdgDriver(iwdgDriver),
+        loggerDriver(loggerDriver),
+        rcDriver(rcDriver),
+		pmDriver(pmDriver),
+        amRCQueue(amRCQueue),
+        tmQueue(tmQueue),
+        smLoggerQueue(smLoggerQueue),
+        smSchedulingCounter(0),
+        batteryLow(false),
+        batteryCritical(false),
+        batteryLowLogged(false),
+        batteryCritLogged(false),
+        batteryVoltage(0.0),
+        batteryCurrent(0.0),
+        batteryDataValid(false),
+        batteryLowCounterMs(0),
+        batteryCritcounterMs(0){}
 
-    private:
-        ISystemUtils *systemUtilsDriver; // System utilities instance
+void SystemManager::smUpdate() {
+    // Kick the watchdog
+    iwdgDriver->refreshWatchdog();
 
-        IIndependentWatchdog *iwdgDriver; // Independent Watchdog driver
-        ILogger *loggerDriver; // Logger driver
-        IRCReceiver *rcDriver; // RC receiver driver
-        IPowerModule *pmDriver;
-        
-        IMessageQueue<RCMotorControlMessage_t> *amRCQueue; // Queue driver for tx communication to the Attitude Manager
-        IMessageQueue<TMMessage_t> *tmQueue; // Queue driver for tx communication to the Telemetry Manager
-        IMessageQueue<char[100]> *smLoggerQueue; // Queue driver for rx communication from other modules to the System Manager for logging
 
-        uint8_t smSchedulingCounter;
+    // Get RC data from the RC receiver and passthrough to AM if new
+    static int oldDataCount = 0;
+    static bool rcConnected = true;
 
-        bool batteryLow;
-        bool batteryCritical;
-        
-        bool batteryLowLogged;
-        bool batteryCritLogged;
+    RCControl rcData = rcDriver->getRCData();
+    if (rcData.isDataNew) {
+        oldDataCount = 0;
+        sendRCDataToAttitudeManager(rcData);
 
-        float batteryVoltage;
-        float batteryCurrent;
-        bool batteryDataValid;
+        if (!rcConnected) {
+            loggerDriver->log("RC Reconnected");
+            rcConnected = true;
+        }
+    } else {
+        oldDataCount += 1;
 
-        //Counter for battery states
-        uint32_t batteryLowCounterMs = 0;
-        uint32_t batteryCritcounterMs = 0;
+        if ((oldDataCount * SM_CONTROL_LOOP_DELAY > SM_RC_TIMEOUT) && rcConnected) {
+            loggerDriver->log("RC Disconnected");
+            rcConnected = false;
+        }
+    }
 
-        void sendRCDataToAttitudeManager(const RCControl &rcData);
-        void sendRCDataToTelemetryManager(const RCControl &rcData);
-        void sendHeartbeatDataToTelemetryManager(uint8_t baseMode, uint32_t customMode, MAV_STATE systemStatus);
-        void sendMessagesToLogger();
-};
+    // Send RC data to TM
+    if (smSchedulingCounter % (SM_SCHEDULING_RATE_HZ / SM_TELEMETRY_RC_DATA_RATE_HZ) == 0) {
+        sendRCDataToTelemetryManager(rcData);
+    }
+
+    // Populate baseMode based on arm state
+    uint8_t baseMode = MAV_MODE_FLAG_MANUAL_INPUT_ENABLED;
+    if (rcData.arm) {
+        baseMode |= MAV_MODE_FLAG_SAFETY_ARMED;
+    }
+
+    // Determine system status based on RC connection and arm state
+    MAV_STATE systemStatus = MAV_STATE_ACTIVE;
+    if (!rcConnected) {
+        systemStatus = MAV_STATE_CRITICAL;
+    } else if (!rcData.arm) {
+        systemStatus = MAV_STATE_STANDBY;
+    }
+
+    // Custom mode not used, set to 0
+    uint32_t customMode = 0;
+
+    // Send Heartbeat data to TM at a 1Hz rate
+    if (smSchedulingCounter % (SM_SCHEDULING_RATE_HZ / SM_TELEMETRY_HEARTBEAT_RATE_HZ) == 0) {
+        sendHeartbeatDataToTelemetryManager(baseMode, customMode, systemStatus);
+    }
+    //Validate pmDriver data and then instantiate voltage and current 
+    if (pmDriver) {
+		PMData_t pmData;
+		bool pmDataValid = pmDriver->readData(&pmData);
+
+        if(pmDataValid){
+            batteryVoltage =  pmData.busVoltage;
+            batteryCurrent = pmData.current;
+            batteryDataValid = true;
+        }
+        else{
+            batteryDataValid = false;
+        }
+	}
+
+    if (batteryDataValid) {
+        //Low battery detection 
+        if (batteryVoltage < BATTERY_LOW_VOLTAGE) {
+            batteryLowCounterMs += SM_CONTROL_LOOP_DELAY;
+            if (batteryLowCounterMs >= BATTERY_LOW_TIME_MS) {
+                batteryLow = true;
+            }
+        } else {
+            batteryLowCounterMs = 0;
+            batteryLow = false;
+            batteryLowLogged = false; 
+        }
+
+        //Crtitical battery detection
+        if (batteryVoltage < BATTERY_CRITICAL_VOLTAGE) {
+            batteryCritcounterMs += SM_CONTROL_LOOP_DELAY;
+            if (batteryCritcounterMs >= BATTERY_CRITICAL_TIME_MS) {
+                batteryCritical = true;
+            }
+        } else {
+            batteryCritcounterMs = 0;
+            batteryCritical = false;
+            batteryCritLogged = false;
+        }
+    }
+
+
+    //Logging --> once per transition, checks if the state has yet to be logged and does so 
+    if(batteryLow &&!batteryLowLogged){
+        loggerDriver->log("Battery low");
+        //TO DO: Send battery data to tm
+        batteryLowLogged = true;
+    }
+
+    if(batteryCritical && !batteryCritLogged){
+        loggerDriver->log("Battery critical");
+        //TO DO: Send battery data to tm
+        batteryCritLogged = true;
+    }
+
+    // Log if new messages
+    if (smLoggerQueue->count() > 0) {
+        sendMessagesToLogger();
+    }
+
+    // Increment scheduling counter
+    smSchedulingCounter = (smSchedulingCounter + 1) % SM_SCHEDULING_RATE_HZ;
+}
+
+void SystemManager::sendRCDataToTelemetryManager(const RCControl &rcData) {
+    TMMessage_t rcDataMsg =  rcDataPack(systemUtilsDriver->getCurrentTimestampMs(), rcData.roll, rcData.pitch, rcData.yaw, rcData.throttle, rcData.aux2, rcData.arm);
+    tmQueue->push(&rcDataMsg);
+}
+
+void SystemManager::sendHeartbeatDataToTelemetryManager(uint8_t baseMode, uint32_t customMode, MAV_STATE systemStatus) {
+    TMMessage_t hbDataMsg = heartbeatPack(systemUtilsDriver->getCurrentTimestampMs(), baseMode, customMode, systemStatus);
+    tmQueue->push(&hbDataMsg);
+}
+
+void SystemManager::sendRCDataToAttitudeManager(const RCControl &rcData) {
+    RCMotorControlMessage_t rcDataMessage;
+
+    rcDataMessage.roll = rcData.roll;
+    rcDataMessage.pitch = rcData.pitch;
+    rcDataMessage.yaw = rcData.yaw;
+    rcDataMessage.throttle = rcData.throttle;
+    rcDataMessage.arm = rcData.arm;
+    rcDataMessage.flapAngle = rcData.aux2;
+
+    amRCQueue->push(&rcDataMessage);
+}
+
+
+void SystemManager::sendMessagesToLogger() {
+    static char messages[16][100];
+    int msgCount = 0;
+
+    while (smLoggerQueue->count() > 0) {
+        smLoggerQueue->get(&messages[msgCount]);
+        msgCount++;
+    }
+
+    loggerDriver->log(messages, msgCount);
+}
