a7383f3453c8c917b81dce44238e98c918cfb091
kariizum <kariizum@uwaterloo.ca>
2026-02-08 20:53:01 -0500

    Initial implementation of airspeed driver

---
+++ stm32l552xx/boardfiles/drivers/airspeed/airspeed.cpp
@@ -0,0 +1,87 @@
+#include "airspeed.hpp"
+#include <cstring>
+
+/*
+airspeedInit - gets the initial data, confirms that data is good
+getData - runs only if airspeedInit gives HAL_OK
+*/
+
+bool airspeed::airspeedInit() {
+    bool success = false;
+    bool dma_success = false;
+    HAL_StatusTypeDef status = HAL_I2C_Master_Receive_DMA(hi2c, devAddress, dmaRXBuffer, arraySize);
+    dma_success = (status == HAL_OK);
+    
+    if (dma_success) {
+        success = callibrate(20, 4);
+    }
+
+    initSuccess_ = success;
+    return success;
+}
+
+bool airspeed::callibrate(int samples, int discard) {
+    double sumPress = 0;
+    double n = 0;
+
+    for (int i = 0; i < samples + discard; i++) {
+        Status status = static_cast<Status>((processRXBuffer[0] >> 6) & 0x03);
+        if (status != Status::Normal) continue;
+        const double rPress = (((processRXBuffer[0] & 0x3F) << 8) | processRXBuffer[1]);
+
+        //calculate pressure in psi
+        //Assumptions: output type A, 30 psi pressure range, differential mode (converted from psi to pa)
+        const double pPress = ((rPress - 1638.3)/6553.2 - 1) * 6894.76;
+
+        if (i < discard) continue;
+
+        sumPress += pPress;
+        n++;
+    }
+
+    if (n < (samples / 2)) return false; // too few good samples
+
+    press_zero = sumPress / n;
+    calibrated_ = true;
+
+    return true;
+}
+
+bool airspeed::getAirspeedData(double* data_out) {
+	if (!initSuccess_) { return false; }
+	return calculateAirspeed(data_out); // Will send a proper error message later
+}
+
+bool airspeed::calculateAirspeed(double* data_out) {
+//    __HAL_I2C_DISABLE_IT(hi2c, I2C_IT_RXI);
+    status_ = static_cast<Status>((processRXBuffer[0] >> 6) & 0x03);
+    if(status_ != Status::Normal) return false; // something wrong with the data
+
+    airspeedData_.raw_press_ = (((processRXBuffer[0] & 0x3F) << 8) | (processRXBuffer[1]));
+    airspeedData_.raw_temp_ = ((processRXBuffer[2] << 3) | (((processRXBuffer[3] & 0xE0) >> 5) & 0x03));
+
+    // -- Calculations -- //
+
+    //process raw temperature and pressure data
+    airspeedData_.processed_temp_ = (airspeedData_.raw_temp_ * 200)/2047 - 50; // calculate temperature in deg C
+
+    //calculate pressure in psi
+    //Assumptions: output type A, 30 psi pressure range, differential mode
+    airspeedData_.processed_press_ = (airspeedData_.raw_press_ - 1638.3)/6553.2 - 1;
+
+    //convert pressure to Pa
+    if (calibrated_) {
+    	airspeedData_.processed_press_ = abs((airspeedData_.processed_press_ * 6894.76) - press_zero);
+    }
+
+
+    double air_density = 101325.0 / (287.058 * (airspeedData_.processed_temp_ + 273.15)); //calculate air density in kg/m^3, assuming stanard air pressure of 101.325 kPa and specific gas constant for dry air R = 287.058 J/(kg·K)
+
+    //calculate airspeed in m/s using Bernoulli's equation
+    airspeedData_.airspeed_ = std::sqrt((2 * airspeedData_.processed_press_) / air_density); // this negative symbol might not be right
+
+//    __HAL_I2C_ENABLE_IT(hi2c, I2C_IT_RXI);
+
+    *data_out = airspeedData_.airspeed_;
+    return true; // Will send a proper error message later
+}
---
+++ stm32l552xx/boardfiles/drivers/airspeed/airspeed.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "stm32l5xx_hal.h"
+#include <cmath>
+
+enum class Status : uint8_t {
+    Normal  = 0b00,
+    Command = 0b01,
+    Stale   = 0b10,
+    Fault   = 0b11
+};
+
+struct airspeedData {
+    double raw_press_ = 0;
+    double raw_temp_ = 0;
+    double processed_temp_ = 0;
+    double processed_press_ = 0;
+    double airspeed_ = 0;
+};
+
+class airspeed : public airspeed_iface
+{
+private:
+	static constexpr int arraySize = 4;
+    uint8_t dmaRXBuffer[arraySize];
+    uint8_t processRXBuffer[arraySize];
+
+    I2C_HandleTypeDef* hi2c;
+    uint8_t devAddress;
+
+    double press_zero = 0.0;
+
+    bool callibrate(int samples, int discard);
+    bool calibrated_ = false;
+    bool initSuccess_ = false;
+
+    Status status_ = Status::Fault;
+
+    airspeedData airspeedData_;
+
+public:
+    airspeed(I2C_HandleTypeDef* i2c, uint8_t addr = 0x28) : hi2c(i2c), devAddress(addr << 1) {}
+    ~airspeed() = default;
+
+    bool airspeedInit();
+    bool getAirspeedData(double* data_out);
+    bool getOffset(bool poll);
+
+    //helper function to calculate airspeed
+    bool calculateAirspeed(double* data_out);
+
+    // public getters
+    uint8_t* getDMARXBuffer() { return dmaRXBuffer; }
+    uint8_t* getProcessRXBuffer() { return processRXBuffer; }
+    uint8_t getDevAddress() { return devAddress; }
+    uint8_t getArraySize() { return arraySize; }
+    airspeedData getAirspeedDataStruct() { return airspeedData_; }
+};
