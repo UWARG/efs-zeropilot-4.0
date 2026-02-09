06099169a1adaa1d85c6d4d7713e8cec48000bcd
SuperMK15 <smartiskush@gmail.com>
2026-02-08 18:13:09 -0800

    commenting

--- zeropilot4.0/include/attitude_manager/flightmode.hpp
+++ zeropilot4.0/include/attitude_manager/flightmode.hpp
@@ -14,6 +14,7 @@ class Flightmode {
         virtual RCMotorControlMessage_t runControl(RCMotorControlMessage_t controlInput, const DroneState_t &droneState) = 0;
 };
 
+// Flight modes for PLANE: numbering aligns to ArduPilot's MAVLink mapping for MissionPlanner compatibility
 enum class PlaneFlightMode_e : uint32_t {
     MANUAL  = 0,
     FBWA    = 5
