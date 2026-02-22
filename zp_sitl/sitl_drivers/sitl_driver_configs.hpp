b822180bb3936795cdb8c93fd69385de2863fdce
SuperMK15 <smartiskush@gmail.com>
2026-02-21 17:49:18 -0800

    fix CURRENT_DRAW_IDLE value

--- zp_sitl/sitl_drivers/sitl_driver_configs.hpp
+++ zp_sitl/sitl_drivers/sitl_driver_configs.hpp
@@ -27,7 +27,7 @@ namespace SITL_Driver_Configs {
 
         static constexpr float BATTERY_INTERNAL_RESISTANCE_OHMS = 0.025f; // Simulated internal resistance of the battery in Ohms
 
-        static constexpr float CURRENT_DRAW_IDLE = 5.0f; // Simulated current draw at idle in Amps
+        static constexpr float CURRENT_DRAW_IDLE = 3.0f; // Simulated current draw at idle in Amps
         static constexpr float CURRENT_DRAW_PER_RPM = 0.01f; // Simulated current draw per RPM in Amps
     };
 
