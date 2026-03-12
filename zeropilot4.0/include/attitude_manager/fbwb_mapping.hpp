afddff97d5bb2c7f3be0f909df86267603e4ec06
SuperMK15 <smartiskush@gmail.com>
2026-03-12 20:09:18 +0000

    Update FBWB skeleton

--- zeropilot4.0/include/attitude_manager/fbwb_mapping.hpp
+++ zeropilot4.0/include/attitude_manager/fbwb_mapping.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "flightmode.hpp"
+#include "fbwa_mapping.hpp"
 #include "pid.hpp"
 
-class FBWBMapping : public Flightmode {
+class FBWBMapping : public FBWAMapping {
     public:
         FBWBMapping(float control_iter_period_s) noexcept;
 
@@ -11,20 +11,15 @@ class FBWBMapping : public Flightmode {
 
         RCMotorControlMessage_t runControl(RCMotorControlMessage_t controlInput, const DroneState_t &droneState) override;
 
-        void setRollPIDConstants(float newKp, float newKi, float newKd, float newTau) noexcept;
-        void setPitchPIDConstants(float newKp, float newKi, float newKd, float newTau) noexcept;
         void setTotalEnergyPIDConstants(float newKp, float newKi, float newKd, float newTau) noexcept;
         void setEnergyBalancePIDConstants(float newKp, float newKi, float newKd, float newTau) noexcept;
-        void setYawRudderMixingConstant(float newMixingConst) noexcept;
 
         void resetControlLoopState() noexcept;
 
         ~FBWBMapping() noexcept override = default;
 
     private:
-        // Roll and Pitch inner PID control loops
-        PID rollPID;
-        PID pitchPID;
+        float calculateSpecificKE(float airspeed_mps);
 
         // TECS outer PID control loops
         PID totalEnergyPID;
@@ -34,13 +29,10 @@ class FBWBMapping : public Flightmode {
         bool isInitialized;
         float targetAltitude_m;
         float targetAirspeed_mps;
-        float currentPitchSetpoint_rad;
+        float currentPitchSetpoint;
         float currentThrottleOutput_pct;
         uint32_t outerLoopSchedulingCounter;
         float dt_s;
-
-        // Yaw rudder mixing constant
-        float yawRudderMixingConst;
 
         // Ratio between scheduling for inner loop and outer loop
         static constexpr uint8_t OUTER_LOOP_DIVIDER = 10;
@@ -48,32 +40,12 @@ class FBWBMapping : public Flightmode {
         // Constants
         static constexpr float GRAVITY_MSS = 9.81f;
 
-        // Output limits (for control effort)
-        static constexpr float OUTPUT_MIN = -1.0f;
-        static constexpr float OUTPUT_MAX = +1.0f;
-
-        // Integral limits (to prevent windup)
-        static constexpr float INTEGRAL_MIN = -0.5f;
-        static constexpr float INTEGRAL_MAX = +0.5f;
-
-        // Roll and Pitch Angle Ranges (in radians)
-        static constexpr float ROLL_MIN_ANGLE_RAD = -0.785f;  // -45 deg
-        static constexpr float ROLL_MAX_ANGLE_RAD = 0.785f;   // +45 deg
-        static constexpr float PITCH_MIN_ANGLE_RAD = -0.349f; // -20 deg
-        static constexpr float PITCH_MAX_ANGLE_RAD = 0.349f;  // +20 deg
-
         // Minimum and Maximum Airspeed Limits (in m/s)
         static constexpr float MIN_AIRSPEED_MPS = 20.0f; // 20 m/s = ~40 knots
         static constexpr float MAX_AIRSPEED_MPS = 62.0f; // 60 m/s = ~120 knots
-        static constexpr float MAX_AIRSPEED_SLEW_RATE_MSS = 2.0f; // Max target speed change (m/s^2)
+        static constexpr float MAX_AIRSPEED_SLEW_RATE_MSS = 1.0f; // Max target speed change (m/s^2)
+        static constexpr float AIRSPEED_WEIGHT = 0.5f; // TECS energy balance weighting of KE relative to PE
 
         // Maximum Altitude Change Rate (in m/s)
         static constexpr float MAX_ALTITUDE_DELTA_MPS = 2.50f; // 2.50m/s = 500 feet/min
-
-        // PID output scale and shift to convert from [-1,1] normalized range to [0,100] motor range
-        static constexpr float FBWB_PID_OUTPUT_SCALE = 50.0f;
-        static constexpr float FBWB_PID_OUTPUT_SHIFT = 50.0f;
-
-        // Assumed normalized range of RC Input to be [0, 100]
-        static constexpr float MAX_RC_INPUT_VAL = 100.0f;
 };
--- zeropilot4.0/src/attitude_manager/fbwb_mapping.cpp
+++ zeropilot4.0/src/attitude_manager/fbwb_mapping.cpp
@@ -1,38 +1,24 @@
 #include "fbwb_mapping.hpp"
 
 FBWBMapping::FBWBMapping(float control_iter_period_s) noexcept :
-    rollPID(0.0f, 0.0f, 0.0f,
-        0.0f, OUTPUT_MIN, OUTPUT_MAX,
-        INTEGRAL_MIN, INTEGRAL_MAX, control_iter_period_s),
-    pitchPID(0.0f, 0.0f, 0.0f,
-        0.0f, OUTPUT_MIN, OUTPUT_MAX,
-        INTEGRAL_MIN, INTEGRAL_MAX, control_iter_period_s),
+    FBWAMapping(control_iter_period_s),
     totalEnergyPID(0.0f, 0.0f, 0.0f,
-        0.0f, OUTPUT_MIN, OUTPUT_MAX,
-        INTEGRAL_MIN, INTEGRAL_MAX, control_iter_period_s * OUTER_LOOP_DIVIDER),
+        0.0f, PID_OUTPUT_MIN, PID_OUTPUT_MAX,
+        PID_INTEGRAL_MIN, PID_INTEGRAL_MAX,
+        control_iter_period_s * OUTER_LOOP_DIVIDER),
     energyBalancePID(0.0f, 0.0f, 0.0f,
-        0.0f, OUTPUT_MIN, OUTPUT_MAX,
-        INTEGRAL_MIN, INTEGRAL_MAX, control_iter_period_s * OUTER_LOOP_DIVIDER),
+        0.0f, PID_OUTPUT_MIN, PID_OUTPUT_MAX,
+        PID_INTEGRAL_MIN, PID_INTEGRAL_MAX,
+        control_iter_period_s * OUTER_LOOP_DIVIDER),
     isInitialized(false),
     targetAltitude_m(0.0f),
-    currentPitchSetpoint_rad(0.0f),
+    currentPitchSetpoint(0.0f),
     currentThrottleOutput_pct(50.0f),
     outerLoopSchedulingCounter(0),
-    dt_s(control_iter_period_s),
-    yawRudderMixingConst(0.0f)
+    dt_s(control_iter_period_s)
 {
-    rollPID.pidInitState();
-    pitchPID.pidInitState();
     totalEnergyPID.pidInitState();
     energyBalancePID.pidInitState();
-}
-
-void FBWBMapping::setRollPIDConstants(float newKp, float newKi, float newKd, float newTau) noexcept {
-    rollPID.setConstants(newKp, newKi, newKd, newTau);
-}
-
-void FBWBMapping::setPitchPIDConstants(float newKp, float newKi, float newKd, float newTau) noexcept {
-    pitchPID.setConstants(newKp, newKi, newKd, newTau);
 }
 
 void FBWBMapping::setTotalEnergyPIDConstants(float newKp, float newKi, float newKd, float newTau) noexcept {
@@ -43,18 +29,14 @@ void FBWBMapping::setEnergyBalancePIDConstants(float newKp, float newKi, float n
     energyBalancePID.setConstants(newKp, newKi, newKd, newTau);
 }
 
-void FBWBMapping::setYawRudderMixingConstant(float newMixingConst) noexcept {
-    yawRudderMixingConst = newMixingConst;
-}
-
 void FBWBMapping::resetControlLoopState() noexcept {
-    rollPID.pidInitState();
-    pitchPID.pidInitState();
+    FBWAMapping::resetControlLoopState();
     totalEnergyPID.pidInitState();
     energyBalancePID.pidInitState();
 }
 
 void FBWBMapping::activateFlightMode() {
+    FBWAMapping::activateFlightMode();
     resetControlLoopState();
     isInitialized = false;
     outerLoopSchedulingCounter = 0;
@@ -63,4 +45,8 @@ void FBWBMapping::activateFlightMode() {
 RCMotorControlMessage_t FBWBMapping::runControl(RCMotorControlMessage_t controlInputs, const DroneState_t &droneState) {
     // TODO: Add FBWB TECS logic, for now is a DirectMapping
     return controlInputs;
+}
+
+float FBWBMapping::calculateSpecificKE(float airspeed_mps) {
+    return (airspeed_mps * airspeed_mps) / (2.0f * GRAVITY_MSS);
 }
