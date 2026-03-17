97de0fb51322050fe0037de2eeefc37ba679c538
kariizum <kariizum@uwaterloo.ca>
2026-03-17 12:09:27 -0400

    Update ioc

--- stm32h753iitx/Core/Inc/stm32h7xx_it.h
+++ stm32h753iitx/Core/Inc/stm32h7xx_it.h
@@ -59,14 +59,15 @@ void DMA1_Stream3_IRQHandler(void);
 void DMA1_Stream4_IRQHandler(void);
 void DMA1_Stream5_IRQHandler(void);
 void DMA1_Stream6_IRQHandler(void);
-void I2C1_EV_IRQHandler(void);
-void I2C1_ER_IRQHandler(void);
+void I2C2_EV_IRQHandler(void);
+void I2C2_ER_IRQHandler(void);
 void SPI1_IRQHandler(void);
 void USART1_IRQHandler(void);
 void USART2_IRQHandler(void);
 void DMA1_Stream7_IRQHandler(void);
 void UART4_IRQHandler(void);
 void TIM6_DAC_IRQHandler(void);
+void DMA2_Stream0_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
