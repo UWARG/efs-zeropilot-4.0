2f978643ba8b0f192995c471f63f98332885e603
Syed <alam.syedmz@gmail.com>
2026-06-19 17:18:05 -0400

    updated sitl systemutils file

--- zp_sitl/sitl_drivers/sitl_systemutils.hpp
+++ zp_sitl/sitl_drivers/sitl_systemutils.hpp
@@ -10,23 +10,21 @@ private:
 public:
     SITL_SystemUtils() : startTime(std::chrono::steady_clock::now()) {}
     
-    ZP_ERROR_e delayMs(uint32_t delay_ms) override {
+    void delayMs(uint32_t delay_ms) override {
         std::this_thread::sleep_for(std::chrono::milliseconds(delay_ms));
-        return ZP_ERROR_OK;
     }
     
-    ZP_ERROR_e getCurrentTimestampMs(uint32_t& currentTime) override {
+    uint32_t getCurrentTimestampMs() override {
         auto now = std::chrono::steady_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - startTime);
-        currentTime = static_cast<uint32_t>(duration.count());
-        return ZP_ERROR_OK;
+        return static_cast<uint32_t>(duration.count());
     }
 
-    ZP_ERROR_e profilerRegister(const char* name, uint8_t* outId) override { return ZP_ERROR_OK; }
+    void profilerRegister(const char* name, uint8_t* outId) override { return; }
 
-    ZP_ERROR_e profilerBegin(uint8_t id) override { return ZP_ERROR_OK; }
+    void profilerBegin(uint8_t id) override { return; }
 
-    ZP_ERROR_e profilerEnd(uint8_t id) override { return ZP_ERROR_OK; }
+    void profilerEnd(uint8_t id) override { return; }
 
-    ZP_ERROR_e profilerGetAll(TaskProfile* out, uint8_t* count) override { return ZP_ERROR_OK; }
+    void profilerGetAll(TaskProfile* out, uint8_t* count) override { return; }
 };
