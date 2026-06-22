144af9a8560eb9ac1e0c1062696ac8a7eee19ab1
KrisKo <143633894+MyeongcheolKo@users.noreply.github.com>
2026-06-22 11:42:46 -0400

    Remove TIM2 DMA Hotfix (#163)
    Remove tim2 DMA channels

--- stm32h753iitx/Core/Inc/stm32h7xx_it.h
+++ stm32h753iitx/Core/Inc/stm32h7xx_it.h
@@ -71,10 +71,6 @@ void DMA2_Stream0_IRQHandler(void);
 void DMA2_Stream1_IRQHandler(void);
 void DMA2_Stream2_IRQHandler(void);
 void DMA2_Stream3_IRQHandler(void);
-void DMA2_Stream4_IRQHandler(void);
-void DMA2_Stream5_IRQHandler(void);
-void DMA2_Stream6_IRQHandler(void);
-void DMA2_Stream7_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
