9b2058dd8a65125931dd8f262bf51469fa389a6f
zhonglin.wang <150401182+ZhonglinGit@users.noreply.github.com>
2026-06-30 21:15:16 -0400

    add virtual com to override

--- stm32h753iitx/boardfiles/override.cpp
+++ stm32h753iitx/boardfiles/override.cpp
@@ -49,6 +49,9 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
     else if (huart == telemLinkHandle->getHUART()) {
       telemLinkHandle->receiveCallback(Size);
     }
+    else if (huart == telemLinkvirtualComHandle->getHUART()) {
+      telemLinkvirtualComHandle->receiveCallback(Size);
+    }
     // GPS dma callback
     else if (huart == gpsHandle->getHUART()) {
       gpsHandle->processGPSData();
