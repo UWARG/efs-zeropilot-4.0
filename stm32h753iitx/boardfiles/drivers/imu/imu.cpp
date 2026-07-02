433da2cd74808a66fc9cc8b0205c099e56e35429
krisko <myeongcheolko@gmail.com>
2026-07-02 19:36:05 -0400

    Fix processing prev data when spi dma not HAL_OK

--- stm32h753iitx/boardfiles/drivers/imu/imu.cpp
+++ stm32h753iitx/boardfiles/drivers/imu/imu.cpp
@@ -52,6 +52,7 @@ int IMU::init() {
 RawImuBatch_t IMU::readRawData() {
     // Dont start another dma transaction when in the middle of one transaction
     if (!dmaDone) {
+        rawImuDataBatch.count = 0;
         return rawImuDataBatch;
     }
     
@@ -180,6 +181,7 @@ void IMU::dmaTransfer() {
                 csHigh();
                 dmaDone = true; // Allow next transfer to be attempted
                 rxFlag = COUNT; // Reset state to COUNT
+                fifoSize = 0;
             }
             break;
 
@@ -192,6 +194,7 @@ void IMU::dmaTransfer() {
                 csHigh();
                 dmaDone = true; // Allow next transfer to be attempted
                 rxFlag = COUNT; // Reset state to COUNT
+                fifoSize = 0;
             }
             break;
 
