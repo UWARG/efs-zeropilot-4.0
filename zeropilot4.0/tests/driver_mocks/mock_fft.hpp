c3cbeef9eafce341b90a60214df79f640ce17a7b
krisko <myeongcheolko@gmail.com>
2026-07-08 14:57:31 -0400

    Fix unit test

--- zeropilot4.0/tests/driver_mocks/mock_fft.hpp
+++ zeropilot4.0/tests/driver_mocks/mock_fft.hpp
@@ -8,4 +8,4 @@ class MockFFT : public IFFT {
         MOCK_METHOD(bool, init, (uint16_t fftLen), (override));
         MOCK_METHOD(void, runFFT, (float* input_buffer, float* output_buffer, uint8_t direction), (override));
         MOCK_METHOD(void, computeMag, (const float* input_buffer, float* output_buffer, uint32_t numSamples), (override));
-}
+};
