488e4bed62adcd46339e20ded230e65aebddc5be
SuperMK15 <smartiskush@gmail.com>
2026-07-08 18:26:29 -0400

    Add CMSIS DSP src files to setup.py

--- zp_sitl/setup.py
+++ zp_sitl/setup.py
@@ -10,7 +10,7 @@ VEHICLE = os.environ.get('V', 'PLANE').upper()
 if VEHICLE not in ('QUADCOPTER', 'PLANE'):
     raise ValueError(f"ZP_VEHICLE must be QUADCOPTER or PLANE, got: {VEHICLE}")
 
-# 1. Handle OS-specific compiler and linker settings
+# Handle OS-specific compiler and linker settings
 if platform.system() == "Windows":
     compile_args = ['/std:c++20', '/D_USE_MATH_DEFINES', '/D_CRT_SECURE_NO_WARNINGS', '/wd4244', '/D__GNUC_PYTHON__', f'/D{VEHICLE}']
     libraries = ['ws2_32']
@@ -18,12 +18,28 @@ else:
     # GCC/Clang Flags
     compile_args = ['-std=c++17', '-D__GNUC_PYTHON__', f'-D{VEHICLE}']
     libraries = []
-    
-sources = ['zeropilot_wrapper.cpp', 'sitl_drivers/sitl_cmsis_dsp.cpp']
+
+# Collect ZeroPilot source files
+sources = ['zeropilot_wrapper.cpp']
 sources += glob.glob(
     os.path.join(zeropilot_root, 'src', '**', '*.cpp'),
     recursive=True
 )
+
+# Add on CMSIS-DSP source files
+sources += [
+    '../external/CMSIS-DSP/Source/TransformFunctions/arm_rfft_fast_f32.c',
+    '../external/CMSIS-DSP/Source/TransformFunctions/arm_rfft_fast_init_f32.c',
+    '../external/CMSIS-DSP/Source/TransformFunctions/arm_cfft_f32.c',
+    '../external/CMSIS-DSP/Source/TransformFunctions/arm_cfft_init_f32.c',
+    '../external/CMSIS-DSP/Source/TransformFunctions/arm_cfft_radix8_f32.c',
+    '../external/CMSIS-DSP/Source/TransformFunctions/arm_bitreversal2.c',
+    '../external/CMSIS-DSP/Source/ComplexMathFunctions/arm_cmplx_mag_f32.c',
+    '../external/CMSIS-DSP/Source/FastMathFunctions/arm_sin_f32.c',
+    '../external/CMSIS-DSP/Source/FastMathFunctions/arm_cos_f32.c',
+    '../external/CMSIS-DSP/Source/CommonTables/arm_common_tables.c',
+    '../external/CMSIS-DSP/Source/CommonTables/arm_const_structs.c',
+]
 
 zeropilot = Extension(
     'zeropilot',
