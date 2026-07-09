07421cd525c097a3700d336686ab5f16303c6453
SirSkye <93548336+SirSkye@users.noreply.github.com>
2026-07-09 09:58:52 +0900

    snake cased

--- stm32h753iitx/boardfiles/drivers/gps/rtkgps.cpp
+++
@@ -1,7 +0,0 @@
-#include "rtkgps.hpp"
-#include "stm32h7xx_hal.h"
-
-HAL_StatusTypeDef RTKGPS::sendCorrectionData(const uint8_t *data, uint16_t len) {
-    if (HAL_UART_Transmit(getHUART(), data, len, HAL_MAX_DELAY) == HAL_OK) return HAL_OK;
-    else return HAL_ERROR;
-};
--- stm32h753iitx/boardfiles/drivers/gps/rtkgps.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "gps.hpp"
-
-class RTKGPS: public GPS {
-public:
-    HAL_StatusTypeDef sendCorrectionData(const uint8_t *data, uint16_t len);
-};
