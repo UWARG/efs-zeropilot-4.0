98e045e60d861901d8d7a266edfa192f7fbee583
Rohan Raina <rohan.raina04@gmail.com>
2026-07-24 22:56:26 -0400

    remove debug values from l5

--- stm32l552xx/boardfiles/rtos/threads/inc/bus_threads.hpp
+++ stm32l552xx/boardfiles/rtos/threads/inc/bus_threads.hpp
@@ -6,8 +6,5 @@
 static constexpr uint16_t BUS_UPDATE_LOOP_DELAY_MS = 50; // 20 Hz
 
 extern osThreadId_t busMainHandle;
-extern volatile uint32_t busMainLoopCount;
-extern volatile uint32_t busFreeHeapBeforeCreate;
-extern volatile uint32_t busFreeHeapAfterCreate;
 
 void busInitThreads();
--- stm32l552xx/boardfiles/rtos/threads/src/bus_threads.cpp
+++ stm32l552xx/boardfiles/rtos/threads/src/bus_threads.cpp
@@ -1,15 +1,10 @@
 #include "bus_threads.hpp"
-#include "can_controller.hpp"
 #include "utils.h"
+#include "drivers.hpp"
 #include "FreeRTOS.h"
 #include "main.h"
 
-extern CANController *canControllerHandle;
-
 osThreadId_t busMainHandle = nullptr;
-volatile uint32_t busMainLoopCount = 0;
-volatile uint32_t busFreeHeapBeforeCreate = 0;
-volatile uint32_t busFreeHeapAfterCreate = 0;
 
 static StaticTask_t busMainControlBlock;
 static StackType_t busMainStack[1024 / sizeof(StackType_t)];
@@ -28,7 +23,6 @@ void busMainLoopWrapper(void *arg)
   uint32_t nextWakeUp = osKernelGetTickCount();
   while(true)
   {
-    busMainLoopCount++;
     if (canControllerHandle) {
       canControllerHandle->routineTasks();
     }
@@ -40,9 +34,7 @@ void busMainLoopWrapper(void *arg)
 
 void busInitThreads()
 {
-    busFreeHeapBeforeCreate = static_cast<uint32_t>(xPortGetFreeHeapSize());
     busMainHandle = osThreadNew(busMainLoopWrapper, NULL, &busMainLoopAttr);
-    busFreeHeapAfterCreate = static_cast<uint32_t>(xPortGetFreeHeapSize());
 
     if (busMainHandle == nullptr) {
         Error_Handler();
