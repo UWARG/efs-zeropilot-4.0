3b605ce17a37fe0d7d81cdcad22c01f6ea261ccd
krisko <myeongcheolko@gmail.com>
2026-08-10 15:30:28 -0400

    Remove units from var names

--- zeropilot4.0/include/driver_ifaces/rangefinder_datatypes.hpp
+++ zeropilot4.0/include/driver_ifaces/rangefinder_datatypes.hpp
@@ -3,9 +3,9 @@
 #include <cstdint>
 
 typedef struct {
-    float distance_m; // m
+    float distance; // m
     uint16_t signalStrength; // Higher is better
-    int16_t temp_C; // Chip temperature in celsius 
+    int16_t temp; // Chip temperature in celsius 
     bool isValid; // True if data is valid, false otherwise
     bool isNew; // True if the data is new since last readData()
 } RangefinderData_t;
